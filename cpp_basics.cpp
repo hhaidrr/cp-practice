6886594e36e573b681dee3dd9c331ccaf591695a
Hamzah Haider <hamzahhaider1@gmail.com>
2025-05-08 09:03:16 -0400

    loops: problem K

--- cpp_basics.cpp
+++ cpp_basics.cpp
@@ -3,21 +3,14 @@
 using namespace std;
 
 int main() {
-    // input x
-    // variable length loop (2 - x-1)
-    // check divisibilty, if true, circuit break, print NO
-    // loop end, print YES
     int x = 0;
 
     cin >> x;
 
-
-    for (int i = 2; i < x; i++){
+    for (int i = 1; i <= x; i++){
         if (x % i == 0){
-            cout << "NO";
-            return 0;
+            cout << i << "\n";
         }
     }
-    cout << "YES";
     return 0;
 }
